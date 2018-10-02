b2b755bcecb41f09dc8fa462ea9993e24d7d230a
Andrew Xie <andrewxie43@gmail.com>
2018-10-01 22:53:52 -0700

    Added constants
    Constants for atmosphere levels up to the stratopause added.

--- isa.java
+++ isa.java
@@ -2,17 +2,50 @@ import java.util.Scanner;
 
 public class isa
 {
-  //density in kg/m^3, pressure in pa, temperature in k
+  //density in kg/m^3, pressure in pa, temperature in k, distance in m
+
+  //input parameters
   private int density;
   private int pressure;
   private int temp;
   private int alt;
 
+  //Sea level/atmosphere constants
   private static int seaLevelDensity = 1.225;
   private static int seaLevelPressure = 101325;
   private static int seaLevelTemp = 288.15;
-
   private static int gasConstant = 287;
+
+
+  //atmosphere layers
+  private static int troposphereMin = 0; //Troposphere
+  private static int troposphereMax = 11000;
+  private static int troposphereTemp = 288.15;
+  private static int troposphereTempChange = -0.0065; //in K/m
+
+  private static int tropopauseMin = 11000; //Tropopause
+  private static int tropopauseMax = 20000;
+  private static int tropopauseTemp = 216.65; //Double Check value
+  private static int tropopauseTempChange = 0;
+
+  private static int lowStratosphereMin = 20000; //Lower Stratosphere
+  private static int lowStratosphereMax = 32000;
+  private static int lowStratosphereTemp = 216.65; //Double Check value
+  private static int lowStratosphereTempChange = 0.001;
+
+  private static int highStratosphereMin = 32000; //Higher Stratosphere
+  private static int highStratosphereMax = 47000;
+  private static int highStratosphereTemp = 228.65; //Double Check value
+  private static int highStratosphereTempChange = 0.0028;
+
+  private static int stratopauseMin = 47000; //Stratopause
+  private static int stratopauseMax = 51000;
+  private static int stratopauseTemp = 270.65; //Double Check value
+  private static int stratopauseTempChange = 0;
+
+
+
+
 
   public isa()
   {
@@ -54,6 +87,8 @@ public class isa
     }
   }
 
+
+  public int
 
 
 
