dbbfd23307845b835c4bbac406613d61cf54362b
Andrew X <andrewxie43@gmail.com>
2018-10-23 10:58:06 -0700

    New test value

--- main.java
+++ main.java
@@ -8,7 +8,9 @@ public class main
   {
     isa w = new isa();
     //System.out.println(df.format(w.findTempAlt(9144))); //test value of 9144m should return 228.714K
-    System.out. println(w.findPressureAlt(9144)); //9144m = 30082.8 Pa
+    System.out.println(w.findTempAlt(2400));
+    System.out.println(w.findPressureAlt(2400)); //9144m = 30082.8 Pa
+    //2400m, 75614.2 Pa, 272.55 K
     //9144 m = .4583 kg/m3
 
   }
