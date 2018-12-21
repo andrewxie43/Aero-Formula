911ab24b12949b6596d6317e533ce1917e90cda5
Andrew Xie <andrewxie43@gmail.com>
2018-12-20 16:11:29 -0800

    Layer

--- layer.java
+++ layer.java
@@ -6,6 +6,10 @@ double gradient;
 double initPress;
 double initDense;
 
+public layer(double ia, double ea){
+  initAlt = ia;
+  endAlt = ea;
+}
   public layer(double ia, double ea, double g){
     initAlt = ia;
     endAlt = ea;
@@ -15,15 +19,31 @@ double initDense;
 
   public double findTempAlt(double alti) //Does not need recursion due to contants used. Eventually recusion should be implemented.
   {
-    double startTemp;
-    double temp;
+    double startTemp = 0;
+    double temp = 0;
 
-    if (initAlt == 0){ //if troposphere
+    if (alti >= 0 && alti < 11000){ //if troposphere
       startTemp = 288.15;
+      gradient = -0.0065
     }
-    else if (initAlt == 11000){
+    else if (alti >= 11000 && alti < 20000){
       layer troposphere = new layer(0,11000,-0.0065);
       startTemp = troposphere.findTempAlt(11000);
+    }
+    else if(alti >= 20000 && alti < 32000){
+      layer tropopause = new layer(11000,20000,0);
+      startTemp = tropopause.findTempAlt(200000);
+    }
+    else if(alti >= 32000 && alti < 47000){
+      layer lowStratosphere = new layer(20000,32000,0.001);
+      startTemp = lowStratosphere.findTempAlt(32000);
+    }
+    else if(alti >= 47000 && alti < 51000){
+      layer highStratosphere = new layer(32000,47000,0.0028);
+      startTemp = highStratosphere.findTempAlt(47000);
+    } else if(alti >= 51000){ //CURRENTLY ONLY GOES TO STRATOPAUSE
+      layer stratopause = new layer(47000,51000,0);
+      startTemp = stratopause.findTempAlt(47000);
     }
 
 
--- main.java
+++ main.java
@@ -8,9 +8,13 @@ public class main
   public static void main(String[] args)
   {
     isa w = new isa();
+    layer q = new layer(0,10000);
     //System.out.println(df.format(w.findTempAlt(9144))); //test value of 9144m should return 228.714K
     System.out.println(w.findTempAlt(9144));
-    System.out.println(w.findPressureAlt(9144)); //9144m = 30082.8 Pa
+    System.out.println(q.findTempAlt(9144));
+
+
+    //9144m = 30082.8 Pa
     //2400m, 75614.2 Pa, 272.55 K
     //9144 m = .4583 kg/m3
 
