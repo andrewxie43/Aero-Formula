3ab9993827595062a6640dce74f753223c95f79a
Andrew Xie <andrewxie43@gmail.com>
2018-12-20 19:34:33 -0800

    Debugging
    Still have no idea why isothermal pressure doesn't work

--- layer.java
+++ layer.java
@@ -22,6 +22,7 @@ public double temp = 0;
 public double startAlt = 0;
 public double tempAlt = 0;
 public double P0 = 0;
+public double startAltTemp;
 //constructors
   public layer (double ea){ //for temporary layers
     initAlt = 0;
@@ -43,35 +44,34 @@ public double P0 = 0;
     if (alti > 0 && alti <= 11000){
       startTemp = 288.15;
       gradient = -0.0065;
-      startAlt = 0;
+      startAltTemp = 0;
       P0 = seaLevelPressure;
     }
     else if (alti > 11000 && alti <= 20000){
       layer troposphere = new layer(0,11000,-0.0065);
       startTemp = troposphere.findTempAlt(11000);
-      startAlt = 11000;
-      endAlt = 11000;
+      startAltTemp = 11000;
       P0 = troposphere.findPressureAlt(11000);
       gradient = 0;
     }
     else if(alti > 20000 && alti <= 32000){
       layer tropopause = new layer(11000,20000,0);
       startTemp = tropopause.findTempAlt(200000);
-      startAlt = 20000;
+      startAltTemp = 20000;
       P0 = tropopause.findPressureAlt(20000);
       gradient = 0.001;
     }
     else if(alti > 32000 && alti <= 47000){
       layer lowStratosphere = new layer(20000,32000,0.001);
       startTemp = lowStratosphere.findTempAlt(32000);
-      startAlt = 32000;
+      startAltTemp = 32000;
       P0 = lowStratosphere.findPressureAlt(32000);
       gradient = 0.0028;
     }
     else if(alti > 47000 && alti <= 51000){
       layer highStratosphere = new layer(32000,47000,0.0028);
       startTemp = highStratosphere.findTempAlt(47000);
-      startAlt = 47000;
+      startAltTemp = 47000;
       P0 = highStratosphere.findPressureAlt(47000);
       gradient = 0;
     }
@@ -97,14 +97,13 @@ public double P0 = 0;
     layer provis = new layer(alti);
     if (gradient != 0)
     {
-      double gar = ((gravity) / (gradient * gasConstant) );//If testing works for 9144, change to use recursion to calculate pressures up to other levels.
+      double gar = ((gravity) / (gradient * gasConstant) );
       double x = (provis.findTempAlt(alti) / startTemp);
       return  P0 * Math.pow(x, gar);
     }
     else
     {//DOES NOT WORK
       double grt = -(gravity/(gasConstant * provis.findTempAlt(alti))) * (alti - startAlt);
-      System.out.println(grt);
       return Math.pow(Math.E, grt);
     }
 
