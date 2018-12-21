55a7efa6d3ea4bb0b2e61d6f26a1e9a7ebb3502a
Andrew Xie <andrewxie43@gmail.com>
2018-12-20 18:50:46 -0800

    FindPressureAlt update
    FindPressureAlt in layer class now works for all layers, via P0!

--- layer.java
+++ layer.java
@@ -20,8 +20,8 @@ public static double gasConstant = 287;
 public double startTemp = 0;
 public double temp = 0;
 public double startAlt = 0;
-
-
+public double tempAlt;
+public double P0;
 //constructors
   public layer(double ia, double ea){
     initAlt = ia;
@@ -33,37 +33,50 @@ public double startAlt = 0;
     gradient = g;
   }
 
- // add function to do the recursive layer var init.
+ //recursive variable initiation
   public void setVars(double alti){
-    if (alti >= 0 && alti < 11000){ //if troposphere
+    if (alti > 0 && alti <= 11000){
       startTemp = 288.15;
       gradient = -0.0065;
       startAlt = 0;
+      tempAlt = 11000;
+      P0 = seaLevelPressure;
     }
-    else if (alti >= 11000 && alti < 20000){
+    else if (alti > 11000 && alti <= 20000){
       layer troposphere = new layer(0,11000,-0.0065);
       startTemp = troposphere.findTempAlt(11000);
       startAlt = 11000;
+      endAlt = 11000;
+      tempAlt = 20000;
+      P0 = troposphere.findPressureAlt(11000);
     }
-    else if(alti >= 20000 && alti < 32000){
+    else if(alti > 20000 && alti <= 32000){
       layer tropopause = new layer(11000,20000,0);
       startTemp = tropopause.findTempAlt(200000);
       startAlt = 20000;
+      tempAlt = 32000;
+      P0 = tropopause.findPressureAlt(20000);
     }
-    else if(alti >= 32000 && alti < 47000){
+    else if(alti > 32000 && alti <= 47000){
       layer lowStratosphere = new layer(20000,32000,0.001);
       startTemp = lowStratosphere.findTempAlt(32000);
       startAlt = 32000;
+      tempAlt = 47000;
+      P0 = lowStratosphere.findPressureAlt(32000);
     }
-    else if(alti >= 47000 && alti < 51000){
+    else if(alti > 47000 && alti <= 51000){
       layer highStratosphere = new layer(32000,47000,0.0028);
       startTemp = highStratosphere.findTempAlt(47000);
       startAlt = 47000;
+      tempAlt = 51000;
+      P0 = highStratosphere.findPressureAlt(47000);
     }
     else if(alti >= 51000){ //CURRENTLY ONLY GOES TO STRATOPAUSE
       layer stratopause = new layer(47000,51000,0);
       startTemp = stratopause.findTempAlt(47000);
       startAlt = 51000;
+      tempAlt = 51000;
+      P0 = stratopause.findPressureAlt(51000);
     }
   }
 
@@ -77,13 +90,12 @@ public double startAlt = 0;
   public double findPressureAlt(double alti)
   {
     setVars(alti);
-
     if (gradient != 0)
     {
 
-      double gar = ((gravity) / (gradient * gasConstant) );//If testing works for 9144, change to use recursion to calculate pressures up to other levels.
+      double gar = ((gravity) / (gradient * gasConstant));//If testing works for 9144, change to use recursion to calculate pressures up to other levels.
       double x = (temp / seaLevelTemp);
-      return seaLevelPressure * Math.pow(x, gar);
+      return  P0 * Math.pow(x, gar);
     }
     else
     {
